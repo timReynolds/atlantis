183a242f7477c17d082c1644c23dd585b4e8a60b
Saket Mittal <s@sakkiii.in>
2026-08-13 12:30:29 +0000

    fix(events): fence routed command execution
    Signed-off-by: Saket Mittal <s@sakkiii.in>
    (cherry picked from commit 6d1aa15e6cb784a93538cbf1b2252ec8f20f8849)

--- server/events/project_command_context_builder.go
+++ server/events/project_command_context_builder.go
@@ -336,6 +336,8 @@ func newProjectCommandContext(ctx *command.Context,
 		SuppressJobOutput:          ctx.SuppressJobOutput,
 		SuppressApplyWebhooks:      ctx.SuppressApplyWebhooks,
 		FailOnMissingDependencies:  ctx.FailOnMissingDependencies,
+		ExecutionLease:             ctx.ExecutionLease,
+		RecoverExternalPlans:       ctx.RecoverExternalPlans,
 	}
 }
 
